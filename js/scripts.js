fdeafb098cdf1cfd4ed6dfa3ea4b18cf0b61ade1
Samantha Butters <61861742+snbutters@users.noreply.github.com>
2020-04-30 03:01:37 -0700

    update scripts.js and index.html to enable radio button functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
+  $("form#animals").submit(function(event) {
 
-  $("#formOne").submit(function(event) {
+    var animal = $("input:radio[name=animal]:checked").val();
 
+    if (animal === "goats") {
+      $('#goats, #pigs, #cows').hide();
+      $('#goats').show();
+    } else if (animal === "pigs") {
+      $('#goats, #pigs, #cows').hide();
+      $('#pigs').show();
+    } else if (animal === "cows") {
+      $('#goats, #pigs, #cows').hide();
+      $('#cows').show();
+    }
 
+    event.preventDefault();
   });
 });
